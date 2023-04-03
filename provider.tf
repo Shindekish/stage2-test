7c5b8648d3e71171f6a5b033d5fd32a06d4c5980
Shindekish <118234388+Shindekish@users.noreply.github.com>
2023-04-03 12:40:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 # Add your S3 backend configuration here
 terraform {
   backend "s3" {
-    S3 Bucket: "3.devops.candidate.exam"
+    Bucket: "3.devops.candidate.exam"
     bucket = "kishor-stage2_test-bucket"
     key    = "kishor.shinde"
     region = "us-south-1"
